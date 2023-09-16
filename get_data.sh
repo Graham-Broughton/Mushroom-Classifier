de7cbca37440207713b1e855873387cfc371c9de
Graham-Broughton <broughton.graham@gmail.com>
2023-09-16 01:42:41 -0700

    pipfile

--- get_data.sh
+++ get_data.sh
@@ -17,39 +17,39 @@ BASE="s3://ml-inat-competition-datasets"
 
 dl_fgvcx_2018() {
     echo "Downloading the FGVCX 2018 data"
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2018/categories.json.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2018/inat2018_locations.zip ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2018/test2018.json.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2018/test2018.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2018/train2018.json.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2018/train_val2018.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2018/val2018.json.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2018/categories.json.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2018/inat2018_locations.zip ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2018/test2018.json.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2018/test2018.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2018/train2018.json.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2018/train_val2018.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2018/val2018.json.tar.gz ./data/raw/
 }
 
 dl_fgvcx_2019() {
     # Downloading the FGVCX 2021 data
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2019/train_val2019.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2019/test2019.json.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2019/val2019.json.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2019/categories.json.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2019/train2019.json.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2019/test2019.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2019/train_val2019.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2019/test2019.json.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2019/val2019.json.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2019/categories.json.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2019/train2019.json.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2019/test2019.tar.gz ./data/raw/
 }
 
 dl_fgvcx_2021() {
     # Downloading the FGVCX 2021 data
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2021/train.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2021/train.json.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2021/val.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/2021/val.json.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2021/train.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2021/train.json.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2021/val.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/2021/val.json.tar.gz ./data/raw/
 }
 
 dl_extras() {
     # Downloading the FGVCX extra data
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/inatloc/iNatLoc.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/newt/newt2021_images.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/newt/newt2021_labels.csv.tar.gz ./data/raw/
-    s5cmd --no-sign-request --numworkers 8 cp $BASE/ssw60/ssw60.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/inatloc/iNatLoc.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/newt/newt2021_images.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/newt/newt2021_labels.csv.tar.gz ./data/raw/
+    s5cmd --no-sign-request cp --concurrency 10 $BASE/ssw60/ssw60.tar.gz ./data/raw/
 }
 ############################################################
 
