29455292d1f0e1063ab9e41746af6b8cb4562247
Graham-Broughton <broughton.graham@gmail.com>
2023-10-12 22:10:47 +0000

    working multiprocessing tfrecords

--- Makefile
+++ Makefile
@@ -4,46 +4,47 @@ SHELL = bash
 # Install the dependencies
 .PHONY: build
 build: Pipfile.lock
-	echo Initializing environment...
+	@echo Initializing environment...
 	@pip install pipenv
 	@mamba install s5cmd
 
 # Download the datasets
 .PHONY: all_datasets
 all_datasets: fgvcx_2018 fgvcx_2019 fgvcx_2021
-	echo "Finished downloading and extracting datasets..."
+	@echo "Finished downloading and extracting datasets..."
 
 fgvcx_2018: build scripts/get_data.sh
-	echo "Downloading datasets fgvcx 2018..."
+	@echo "Downloading datasets fgvcx 2018..."
 	@bash scripts/get_data.sh -y 2018
-	echo "Extracting datasets..."
+	@echo "Extracting datasets..."
 	@for f in data/raw/2018/*.tar.gz; do tar -xvf $${f} -C data/raw/2018/; done;
-	echo "Removing tar.gz files..."
+	@echo "Removing tar.gz files..."
 	@rm -rf data/raw/2018/*.tar.gz
-	echo "Finished extracting datasets..."
+	@echo "Finished extracting datasets..."
 
 fgvcx_2019: build scripts/get_data.sh
-	echo "Downloading datasets fgvcx 2019..."
+	@echo "Downloading datasets fgvcx 2019..."
 	@bash scripts/get_data.sh -y 2019
-	echo "Extracting datasets..."
+	@echo "Extracting datasets..."
 	@for f in data/raw/2019/*.tar.gz; do tar -xzf $${f} -C data/raw/2019/; done
 	@rm -rf data/raw/2019/*.tar.gz
-	echo "Finished extracting datasets..."
+	@echo "Finished extracting datasets..."
 
 fgvcx_2021: build scripts/get_data.sh
-	echo "Downloading datasets fgvcx 2021..."
+	@echo "Downloading datasets fgvcx 2021..."
 	@bash scripts/get_data.sh -y 2021
-	echo "Extracting datasets..."
+	@echo "Extracting datasets..."
 	@for f in data/raw/2021/*.tar.gz; do tar -xzf $${f} -C data/raw/2021/; done
 	@rm -rf data/raw/2021/*.tar.gz
-	echo "Finished extracting datasets..."
+	@echo "Finished extracting datasets..."
 
-IMG_DIR ?= $(shell bash -c 'read -p "Where are the images located (optional, default: ./data/)? " image_path; echo $$image_path')
-TFREC_DIR ?= $(shell bash -c 'read -p "Where should the tfrecords be saved (optional, default: ./data/)? " tfrecord_path; echo $$tfrecord_path')
-TRAIN_RECS ?= $(shell bash -c 'read -p "Number of train images per tfrecord (default: 107)? " num_train_records; echo $$num_train_records')
-VAL_RECS ?= $(shell bash -c 'read -p "Number of validation images per tfrecord (default: 5)? " num_val_records; echo $$num_val_records')
-IMG_SIZES ?= $(shell bash -c 'read -p "Image height and width (default: 224, 224)? " image_size; echo $$image_size')
-tfrecords: scripts/create_tfrecords.sh
-	echo "Creating tfrecords..."
-	@bash scripts/create_tfrecords.sh -d $(IMG_DIR) -p $(TFREC_DIR) -t $(TRAIN_RECS) -v $(VAL_RECS) -s $(IMG_SIZES)
-	echo "Finished creating tfrecords..."
+IMG_DIR ?= $(shell bash -c 'read -p "Where are the images located (OPTIONAL, default: ./data/)? " image_path; echo $$image_path')
+TFREC_DIR ?= $(shell bash -c 'read -p "Where should the tfrecords be saved (OPTIONAL, default: ./data/)? " tfrecord_path; echo $$tfrecord_path')
+TRAIN_RECS ?= $(shell bash -c 'read -p "Number of train image tfrecords (OPTIONAL, default: 107)? " num_train_records; echo $$num_train_records')
+VAL_RECS ?= $(shell bash -c 'read -p "Number of validation image tfrecords (OPTIONAL, default: 5)? " num_val_records; echo $$num_val_records')
+IMG_SIZES ?= $(shell bash -c 'read -p "Image height and width (REQUIRED, default: 224, 224)? " image_size; echo $$image_size')
+MULTIPROCESSING ?= $(shell bash -c 'read -p "Use multiprocessing (OPTIONAL, default: True)? " multiprocessing; echo $$multiprocessing')
+tfrecords: build scripts/create_tfrecords.sh
+	@echo "Creating tfrecords..."
+	@bash scripts/create_tfrecords.sh -d $(IMG_DIR) -p $(TFREC_DIR) -t $(TRAIN_RECS) -v $(VAL_RECS) -s $(IMG_SIZES) -m $(MULTIPROCESSING)
+	@echo "Finished creating tfrecords..."
--- src/data_processing/tfrecords.py
+++ src/data_processing/tfrecords.py
@@ -1,14 +1,21 @@
+from os import environ
+from loguru import logger
 import cv2
 import pandas as pd
-import tensorflow as tf
+import warnings
+from tqdm import trange
 
+environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+import tensorflow as tf
 from config import CFG
+
+warnings.filterwarnings("ignore")
 
 AUTO = tf.data.experimental.AUTOTUNE
 
 
 def load_dataframe(root_path):
-    print(root_path)
     df = pd.read_csv(root_path / "train.csv")
     val = df[df["set"] == "val"].sample(frac=1, random_state=42).reset_index(drop=True)
     train = (
@@ -36,17 +43,16 @@ def int64_feature(value):
 
 
 def serialize_example(
-    feature0, feature1, feature2, feature3, feature4, feature5, feature6, feature7
+    feature0, feature1, feature2, feature3, feature4, feature5, feature6
 ):
     feature = {
         "image": bytes_feature(feature0),
         "dataset": int64_feature(feature1),
-        "set": bytes_feature(feature2),
-        "longitude": float_feature(feature3),
-        "latitude": float_feature(feature4),
-        "norm_date": float_feature(feature5),
-        "class_priors": float_feature(feature6),
-        "class_id": int64_feature(feature7),
+        "longitude": float_feature(feature2),
+        "latitude": float_feature(feature3),
+        "norm_date": float_feature(feature4),
+        "class_priors": float_feature(feature5),
+        "class_id": int64_feature(feature6),
     }
     example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
     return example_proto.SerializeToString()
@@ -59,23 +65,15 @@ def get_data(df, num_records=107):
     return IMGS, SIZE, CT
 
 
-def write_records(
-    df, set, tfrec_path, img_path, num_records, reshape_sizes
-):
-    print(tfrec_path, img_path, num_records, reshape_sizes)
+def write_records(df, set, tfrec_path, img_path, num_records, reshape_sizes):
     IMGS, SIZE, CT = get_data(df, num_records)
-    for j in range(CT):
-        print()
-        print(f"Writing TFRecord {j} of {CT}...")
+    for j in trange(CT):
         CT2 = min(SIZE, len(IMGS) - j * SIZE)
-        if not (tfrec_path / "tfrecords-jpeg-{reshape_sizes[0]}x{reshape_sizes[1]}").is_dir():
-            (tfrec_path / "tfrecords-jpeg-{reshape_sizes[0]}x{reshape_sizes[1]}").mkdir(
-                parents=True, exist_ok=True
-            )
-        with tf.io.TFRecordWriter(
-            str(tfrec_path / "tfrecords-jpeg-{reshape_sizes[0]}x{reshape_sizes[1]}" / f"{set}{j:02d}-{CT2}.tfrec")
-        ) as writer:
-            for k in range(CT2):
+        path = tfrec_path / f"tfrecords-jpeg-{reshape_sizes[0]}x{reshape_sizes[1]}"
+        if not path.is_dir():
+            path.mkdir(parents=True, exist_ok=True)
+        with tf.io.TFRecordWriter(str(path / f"{set}{j:02d}-{CT2}.tfrec")) as writer:
+            for k in trange(CT2, leave=False):
                 img = cv2.imread(str(img_path) + f"/{IMGS[SIZE * j + k]}")
                 img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
                 img = cv2.resize(
@@ -94,17 +92,90 @@ def write_records(
                     row.class_id.values[0],
                 )
                 writer.write(example)
-                if k % 100 == 0:
-                    print(k, ", ", end="")
+
+
+def get_mp_params(tfrec_path, img_path, num_records, reshape_sizes, set, df):
+
+    IMGS, SIZE, CT = get_data(df, num_records)
+    param_list = []
+    for j in range(CT):
+        CT2 = min(SIZE, len(IMGS) - j * SIZE)
+        path = tfrec_path / f"tfrecords-jpeg-{reshape_sizes[0]}x{reshape_sizes[1]}"
+        if not path.is_dir():
+            path.mkdir(parents=True, exist_ok=True)
+        image_paths = []
+        image_filenames = []
+        for k in range(CT2):
+            img_filename = IMGS[SIZE * j + k]
+            image_filenames.append(img_filename)
+            image_path = str(img_path / img_filename)
+            image_paths.append(image_path)
+        params = {
+            "df": df,
+            "path": str(path / f"{set}{j:02d}-{CT2}.tfrec"),
+            "reshape_sizes": reshape_sizes,
+            "image_paths": image_paths,
+            "image_filenames": image_filenames,
+        }
+        param_list.append(params)
+    return param_list
+
+
+def write_mp(**kwargs):
+    from tqdm.contrib.concurrent import process_map
+    train, val = load_dataframe(root_path=kwargs['img_directory'])
+    plist = []
+    for df, set in zip([train, val], ["train", "val"]):
+        params = get_mp_params(
+            kwargs['tfrecords_directory'],
+            kwargs['img_directory'],
+            kwargs['num_train_records'] if set == "train" else kwargs['num_validation_records'],
+            kwargs['img_size'],
+            set,
+            df,
+        )
+        plist.append(params)
+    for params in plist:
+        process_map(write_tfrecord_mp, params, max_workers=8)
+
+
+def write_tfrecord_mp(params):
+    df = params["df"]
+    path = params["path"]
+    reshape_sizes = params["reshape_sizes"]
+    image_paths = params["image_paths"]
+    image_filenames = params["image_filenames"]
+
+    with tf.io.TFRecordWriter(path) as writer:
+        for idx, (image_path, image_filename) in enumerate(zip(image_paths, image_filenames)):
+            img = cv2.imread(image_path)
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            img = cv2.resize(
+                img, reshape_sizes, cv2.INTER_CUBIC
+            )  # Fix incorrect colors
+            img = cv2.imencode(".jpg", img)[1].tobytes()
+            row = df.loc[df.file_path == image_filename]
+            example = serialize_example(
+                img,
+                row.dataset.values[0],
+                row.longitude.values[0],
+                row.latitude.values[0],
+                row.norm_date.values[0],
+                row.class_priors.values[0],
+                row.class_id.values[0],
+            )
+            writer.write(example)
 
 
 if __name__ == "__main__":
     import argparse
+
     CFG = CFG()
 
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
-        "-d", "--img-directory",
+        "-d",
+        "--img-directory",
         default=CFG.DATA,
         const=CFG.DATA,
         nargs="?",
@@ -112,7 +183,8 @@ if __name__ == "__main__":
         help="Image base location",
     )
     argparser.add_argument(
-        "-p", "--tfrecords-directory",
+        "-p",
+        "--tfrecords-directory",
         default=CFG.DATA,
         const=CFG.DATA,
         nargs="?",
@@ -120,35 +192,49 @@ if __name__ == "__main__":
         help="tfrecords wanted location",
     )
     argparser.add_argument(
-        "-t", "--num-train-records",
+        "-t",
+        "--num-train-records",
         type=int,
         nargs="?",
         default=CFG.NUM_TRAINING_IMAGES,
         help="number of train records",
     )
     argparser.add_argument(
-        "-v", "--num-validation-records",
+        "-v",
+        "--num-validation-records",
         type=int,
         nargs="?",
         default=CFG.NUM_VALIDATION_IMAGES,
         help="number of validation records",
     )
     argparser.add_argument(
-        "-s", "--img-size", 
-        type=int, 
-        nargs=2, 
-        default=CFG.IMAGE_SIZE, 
-        help="image size"
+        "-s", "--img-size", type=int, nargs=2, default=CFG.IMAGE_SIZE, help="image size"
+    )
+    argparser.add_argument(
+        "-m",
+        "--multiprocessing",
+        action="store_false",
+        help="whether to use multiprocessing",
     )
     args = argparser.parse_args()
-
-    train, val = load_dataframe(root_path=args.img_directory)
-    for df, set in zip([train, val], ["train", "val"]):
-        write_records(
-            df,
-            set,
-            args.tfrecords_directory,
-            args.img_directory,
-            args.num_train_records if set == "train" else args.num_validation_records,
-            reshape_sizes=args.img_size,
+    print(args.img_directory)
+    if not args.multiprocessing:
+        # d = vars(args)
+        write_mp(
+            tfrecords_directory = args.tfrecords_directory,
+            img_directory = args.img_directory,
+            num_train_records = args.num_train_records,
+            num_validation_records = args.num_validation_records,
+            img_size = args.img_size,
         )
+    else:
+        train, val = load_dataframe(root_path=args.img_directory)
+        for df, set in zip([train, val], ["train", "val"]):
+            write_records(
+                df,
+                set,
+                args.tfrecords_directory,
+                args.img_directory,
+                args.num_train_records if set == "train" else args.num_validation_records,
+                reshape_sizes=args.img_size,
+            )
