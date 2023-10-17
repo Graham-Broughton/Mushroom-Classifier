6f0b2d3ee85e0533f337bde4d02201b1e948160d
Graham-Broughton <broughton.graham@gmail.com>
2023-10-17 10:45:57 -0700

    working TPU training

--- src/optimizers/lion.py
+++ src/optimizers/lion.py
@@ -1,94 +1,104 @@
-from typing import Callable, Optional, Tuple
+# Copyright 2023 Google Research. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""TF2 implementation of the Lion optimizer."""
 
-import tensorflow as tf
-from tensorflow.keras import optimizers
-
-# functions
-
-
-def exists(val):
-    return val is not None
-
-
-@tf.function(jit_compile=True)
-def update_fn(p, grad, exp_avg, lr, wd, beta1, beta2):
-    # stepweight decay
-
-    p.assign(p * (1 - lr * wd))
-
-    # weight update
-
-    update = (
-        tf.raw_ops.LinSpace(start=1.0, stop=0.0, num=1, name=None)[0] * exp_avg
-        + (1 - tf.raw_ops.LinSpace(start=1.0, stop=0.0, num=1, name=None)[0]) * grad
-    )
-    p.assign_add(tf.sign(update) * -lr)
-
-    # decay the momentum running average coefficient
-
-    exp_avg.assign(exp_avg * beta2 + grad * (1 - beta2))
+import tensorflow.compat.v2 as tf
 
 
-class Lion(optimizers.Optimizer):
-    def __init__(
-        self,
-        lr: float = 1e-4,
-        betas: Tuple[float, float] = (0.9, 0.99),
-        weight_decay: float = 0.0,
-        use_triton: bool = False,
-        **kwargs
-    ):
-        assert all([0.0 <= beta <= 1.0 for beta in betas])
+class Lion(tf.keras.optimizers.legacy.Optimizer):
+  r"""Optimizer that implements the Lion algorithm."""
 
-        super().__init__(**kwargs)
+  def __init__(self,
+               learning_rate=0.0001,
+               beta_1=0.9,
+               beta_2=0.99,
+               wd=0,
+               name='lion',
+               **kwargs):
+    """Construct a new Lion optimizer."""
 
-        self.lr = lr
-        self.betas = betas
-        self.weight_decay = weight_decay
+    super(Lion, self).__init__(name, **kwargs)
+    self._set_hyper('learning_rate', kwargs.get('lr', learning_rate))
+    self._set_hyper('beta_1', beta_1)
+    self._set_hyper('beta_2', beta_2)
+    self._set_hyper('wd', wd)
 
-        self.update_fn = update_fn
+  def _create_slots(self, var_list):
+    # Create slots for the first and second moments.
+    # Separate for-loops to respect the ordering of slot variables from v1.
+    for var in var_list:
+      self.add_slot(var, 'm')
 
-    def get_config(self):
-        config = super().get_config()
-        config.update(
-            {"lr": self.lr, "betas": self.betas, "weight_decay": self.weight_decay}
-        )
-        return config
+  def _prepare_local(self, var_device, var_dtype, apply_state):
+    super(Lion, self)._prepare_local(var_device, var_dtype, apply_state)
 
-    @tf.function(jit_compile=True)
-    def _resource_apply_dense(self, grad, var):
-        lr = self.lr
-        beta1 = self.betas[0]
-        beta2 = self.betas[1]
-        wd = self.weight_decay
+    beta_1_t = tf.identity(self._get_hyper('beta_1', var_dtype))
+    beta_2_t = tf.identity(self._get_hyper('beta_2', var_dtype))
+    wd_t = tf.identity(self._get_hyper('wd', var_dtype))
+    lr = apply_state[(var_device, var_dtype)]['lr_t']
+    apply_state[(var_device, var_dtype)].update(
+        dict(
+            lr=lr,
+            beta_1_t=beta_1_t,
+            one_minus_beta_1_t=1 - beta_1_t,
+            beta_2_t=beta_2_t,
+            one_minus_beta_2_t=1 - beta_2_t,
+            wd_t=wd_t))
 
-        # init state - exponential moving average of gradient values
-        exp_avg = self.get_slot(var, "exp_avg")
-        if exp_avg is None:
-            exp_avg = self.add_slot(var, "exp_avg", tf.zeros_like(var))
+  @tf.function(jit_compile=True)
+  def _resource_apply_dense(self, grad, var, apply_state=None):
+    var_device, var_dtype = var.device, var.dtype.base_dtype
+    coefficients = ((apply_state or {}).get((var_device, var_dtype)) or
+                    self._fallback_apply_state(var_device, var_dtype))
 
-        self.update_fn(var, grad, exp_avg, lr, wd, beta1, beta2)
+    m = self.get_slot(var, 'm')
+    var_t = var.assign_sub(
+        coefficients['lr_t'] *
+        (tf.math.sign(m * coefficients['beta_1_t'] +
+                      grad * coefficients['one_minus_beta_1_t']) +
+         var * coefficients['wd_t']))
+    with tf.control_dependencies([var_t]):
+      m.assign(m * coefficients['beta_2_t'] +
+               grad * coefficients['one_minus_beta_2_t'])
 
-    # @tf.function(jit_compile=True)
-    # def _resource_apply_sparse(self, grad, var, indices, apply_state=None):
-    #     var_device, var_dtype = var.device, var.dtype.base_dtype
-    #     coefficients = (apply_state or {}).get(
-    #         (var_device, var_dtype)
-    #     ) or self._fallback_apply_state(var_device, var_dtype)
+  @tf.function(jit_compile=True)
+  def _resource_apply_sparse(self, grad, var, indices, apply_state=None):
+    var_device, var_dtype = var.device, var.dtype.base_dtype
+    coefficients = ((apply_state or {}).get((var_device, var_dtype)) or
+                    self._fallback_apply_state(var_device, var_dtype))
 
-    #     m = self.get_slot(var, "exp_avg")
-    #     m_t = m.assign(m * coefficients["beta_1_t"])
-    #     m_scaled_g_values = grad * coefficients["one_minus_beta_1_t"]
-    #     m_t = m_t.scatter_add(tf.IndexedSlices(m_scaled_g_values, indices))
-    #     var_t = var.assign_sub(
-    #         coefficients["lr"] * (tf.math.sign(m_t) + var * coefficients["wd_t"])
-    #     )
+    m = self.get_slot(var, 'm')
+    m_t = m.assign(m * coefficients['beta_1_t'])
+    m_scaled_g_values = grad * coefficients['one_minus_beta_1_t']
+    m_t = m_t.scatter_add(tf.IndexedSlices(m_scaled_g_values, indices))
+    var_t = var.assign_sub(coefficients['lr'] *
+                           (tf.math.sign(m_t) + var * coefficients['wd_t']))
 
-    #     with tf.control_dependencies([var_t]):
-    #         m_t = m_t.scatter_add(tf.IndexedSlices(-m_scaled_g_values, indices))
-    #         m_t = m_t.assign(m_t * coefficients["beta_2_t"] / coefficients["beta_1_t"])
-    #         m_scaled_g_values = grad * coefficients["one_minus_beta_2_t"]
-    #         m_t.scatter_add(tf.IndexedSlices(m_scaled_g_values, indices))
+    with tf.control_dependencies([var_t]):
+      m_t = m_t.scatter_add(tf.IndexedSlices(-m_scaled_g_values, indices))
+      m_t = m_t.assign(m_t * coefficients['beta_2_t'] /
+                       coefficients['beta_1_t'])
+      m_scaled_g_values = grad * coefficients['one_minus_beta_2_t']
+      m_t.scatter_add(tf.IndexedSlices(m_scaled_g_values, indices))
 
-    def _resource_apply_sparse_duplicate_indices(self, grad, var, indices):
-        raise NotImplementedError("Sparse gradient updates are not supported.")
+  def get_config(self):
+    config = super(Lion, self).get_config()
+    config.update({
+        'learning_rate': self._serialize_hyperparameter('learning_rate'),
+        'beta_1': self._serialize_hyperparameter('beta_1'),
+        'beta_2': self._serialize_hyperparameter('beta_2'),
+        'wd': self._serialize_hyperparameter('wd'),
+    })
+    return config
