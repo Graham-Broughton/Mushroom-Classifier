66934e644f991649c2511cc095d36b53f7d48227
Graham-Broughton <broughton.graham@gmail.com>
2023-11-18 01:26:14 +0000

    fixed tfrecords

--- training/src/__init__.py
+++ training/src/__init__.py
@@ -1,11 +1,12 @@
-from src.training.dataset import get_training_dataset, get_validation_dataset
-from src.training.NN import create_model, create_optimizer, make_callbacks
-from src.training.utils import (
+from training.src.training.dataset import get_training_dataset, get_validation_dataset
+from training.src.training.NN import create_model, create_optimizer, make_callbacks
+from training.src.training.utils import (
     count_data_items,
     get_new_cfg,
     select_dataset,
-    set_seed,
+    seed_all,
     tpu_test,
 )
-from src.visuals.training_viz import display_batch_of_images
-from src.training.pathrecordsconverter import PathRecordsConverter
+from training.src.visuals.training_viz import display_batch_of_images
+from training.src.training.pathrecordsconverter import PathRecordsConverter
+from training.src.data_processing import tfrecords
